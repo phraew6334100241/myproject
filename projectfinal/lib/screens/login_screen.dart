be7a6940402f495ad001ea18d0d1a50c32ba77d5
phraew6334100241 <76487352+phraew6334100241@users.noreply.github.com>
2023-04-03 03:19:26 +0700

    project

--- projectfinal/lib/screens/login_screen.dart
+++ projectfinal/lib/screens/login_screen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:projectfinal/main.dart';
 import 'package:projectfinal/screens/create_account_screen.dart';
-import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,63 +8,112 @@ class LoginScreen extends StatelessWidget {
   TextEditingController _passwordController = TextEditingController();
 
   AuthService _service = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Application บันทึกประวัติคนไข้ทั่วไป"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(28.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(hintText: "Email"),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(hintText: "Password"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  bool res = await _service.login(
-                      email: _emailController.text,
-                      password: _passwordController.text);
+      body: Center(
+        child: SizedBox(
+          height: 300,
+          width: 550,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      bool res = await _service.login(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      );
 
-                  print(res);
-                  if (res) {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text("Login Success")));
-                    // ignore: use_build_context_synchronously
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
+                      print(res);
+                      if (res) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Login Success")),
+                        );
+                        // ignore: use_build_context_synchronously
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
                             builder: (context) => MyHomePage(
-                                  title: "รายละเอียดคนไข้ทั่วไป",
-                                )));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Email/Password is Wrong")));
-                  }
-                },
-                child: Text("เข้าสู่ระบบ")),
-            const SizedBox(
-              height: 20,
+                              title: "รายละเอียดคนไข้ทั่วไป",
+                            ),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Email/Password is Wrong")),
+                        );
+                      }
+                    },
+                    child: Text("เข้าสู่ระบบ"),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CreateAccountScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text("สร้างบัญชีใหม่ ใช่หรือไม่? ->"),
+                  ),
+                ],
+              ),
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => CreateAccountScreen()));
-              },
-              child: const Text("สร้างบัญชีใหม่ ใช่หรือไม่? ->"),
-            ),
-          ],
+          ),
         ),
       ),
     );
